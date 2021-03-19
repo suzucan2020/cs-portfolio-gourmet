2e37388e9901c8ee3e351d74dd786f05d1ecb996
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-19 14:54:30 +0000

    update axios.vue

--- pages/axios.vue
+++ pages/axios.vue
@@ -1,11 +1,21 @@
 <template>
   <section class="container">
+    <h1>Print All</h1>
     <div>
       {{ users }}
     </div>
+    <h1>Print id 1</h1>
     <dir>
       {{ users[0].id }}, {{ users[0].name }}
     </dir>
+    <h1>Print v-for</h1>
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }} , {{ user.name }}
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 

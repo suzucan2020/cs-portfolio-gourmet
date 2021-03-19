84b741e5d2fa8420d1e01e169c0285c19e42845f
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-19 16:02:27 +0000

    add hello.js

--- pages/vuex.vue
+++ pages/vuex.vue
@@ -1,10 +1,15 @@
 <template>
   <section class="container">
     <div>
-      <p>{{ $store.state.message }}</p>
+
+      <!-- <p>{{ $store.state.message }}</p> -->
       <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
       <!-- <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
-      <button v-on:click="$store.dispatch('updateMessageAction')">Dispatch</button>
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
+
+      <p>{{ $store.state.hello.message }}</p>
+      <button v-on:click="$store.dispatch('hello/updateMessageAction', 'hello dispatch')">Dispatch</button> -->
+
     </div>
   </section>
 </template>

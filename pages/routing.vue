0fe3f8b2e08590b26899dff3eba5ffb8a54dd42f
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-19 13:20:25 +0000

    add routing

--- pages/routing.vue
+++ pages/routing.vue
@@ -1,5 +1,10 @@
 <template>
   <section class="container">
     <h1>Routing Page</h1>
+    <hr>
+    <router-link to="/">Top Page</router-link>
+    <hr>
+    <router-link to=/card>Card Page</router-link>
   </section>
 </template>
+card.vue

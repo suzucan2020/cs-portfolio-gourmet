968033e6f41be4b5a788004e734781a73272f89b
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-20 00:55:06 +0000

    add counter

---
+++ pages/counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <div>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+    </div>
+  </section>
+</template>
+
+<script>
+import  Counter from '~/components/Counter.vue'
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
+

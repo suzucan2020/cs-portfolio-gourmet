d63ed6710a9954c6fd9ba3fafb9601cfcf7707b8
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-27 08:34:03 +0000

    change footer color

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,7 +10,7 @@
       <v-btn
         v-for="link in links"
         :key="link"
-        color="white"
+        color="black"
         text
         rounded
         class="my-2"
@@ -18,7 +18,7 @@
         {{ link }}
       </v-btn>
       <v-col
-        class="primary lighten-2 py-4 text-center white--text"
+        class="green lighten-4 py-4 text-center black--text"
         cols="12"
       >
         {{ new Date().getFullYear() }} —`<strong>Vuetify</strong>

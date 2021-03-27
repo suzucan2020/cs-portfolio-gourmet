c10b8b97998c73f9222574d15527727161d92d69
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-27 14:41:30 +0000

    update

--- pages/carousels.vue
+++ pages/carousels.vue
@@ -2,7 +2,6 @@
   <v-carousel
     cycle
     interval="3000"
-    hide-delimiters
     show-arrows-on-hover
   >
     <v-carousel-item
@@ -21,16 +20,19 @@
       return {
         items: [
           {
-            src: 'https://picsum.photos/1920/1080?random',
+            src: 'https://picsum.photos/1920/1080?random=1',
           },
           {
-            src: 'https://picsum.photos/1920/1080?random',
+            src: 'https://picsum.photos/1920/1080?random=2',
           },
           {
-            src: 'https://picsum.photos/1920/1080?random',
+            src: 'https://picsum.photos/1920/1080?random=3',
           },
           {
-            src: 'https://picsum.photos/1920/1080?random',
+            src: 'https://picsum.photos/1920/1080?random=4',
+          },
+          {
+            src: 'https://picsum.photos/1920/1080?random=5',
           },
         ],
       }

4419c7f18ee53ee2e04e221a07fec3765bce2410
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-27 15:02:26 +0000

    add sample site

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,8 @@
     <h1>Link</h1>
     <hr>
     <NuxtLink to="/">Top Page</NuxtLink>
+    <hr>
+    <NuxtLink to="/sample-site-000">Sample Site 000</NuxtLink>
     <h1>Vuetify UI component</h1>
     <hr>
     <NuxtLink to=/carousels000>Carousels 000 Page</NuxtLink>
---
+++ pages/sample-site-000.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-carousel
+    cycle
+    interval="3000"
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      Top western road trips
+    </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://picsum.photos/1920/1080?random=1',
+          },
+          {
+            src: 'https://picsum.photos/1920/1080?random=2',
+          },
+          {
+            src: 'https://picsum.photos/1920/1080?random=3',
+          },
+          {
+            src: 'https://picsum.photos/1920/1080?random=4',
+          },
+          {
+            src: 'https://picsum.photos/1920/1080?random=5',
+          },
+        ],
+        show: false,
+      }
+    },
+  }
+</script>

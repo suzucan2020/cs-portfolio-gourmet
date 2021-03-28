379032fabc9d4716d3ee9de47fb0337eb5dd6bc5
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-03-28 08:30:07 +0000

    update

---
+++ components/OurTeam.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-subheader>Our Team</v-subheader>
+
+      <v-expansion-panels popout>
+        <v-expansion-panel
+          v-for="(message, i) in messages"
+          :key="i"
+          hide-actions
+        >
+          <v-expansion-panel-header>
+            <v-row
+              align="center"
+              class="spacer"
+              no-gutters
+            >
+              <v-col
+                cols="4"
+                sm="2"
+                md="1"
+              >
+                <v-avatar
+                  size="36px"
+                >
+                  <img
+                    v-if="message.avatar"
+                    alt="Avatar"
+                    src="https://picsum.photos/1920/1080?random=6"
+                  >
+                  <v-icon
+                    v-else
+                    :color="message.color"
+                    v-text="message.icon"
+                  ></v-icon>
+                </v-avatar>
+              </v-col>
+
+              <v-col
+                class="hidden-xs-only"
+                sm="5"
+                md="3"
+              >
+                <strong v-html="message.name"></strong>
+                <span
+                  v-if="message.total"
+                  class="grey--text"
+                >
+                  &nbsp;({{ message.total }})
+                </span>
+              </v-col>
+
+              <v-col
+                class="text-no-wrap"
+                cols="5"
+                sm="3"
+              >
+                <v-chip
+                  v-if="message.new"
+                  :color="`${message.color} lighten-4`"
+                  class="ml-0 mr-2 black--text"
+                  label
+                  small
+                >
+                  {{ message.new }} new
+                </v-chip>
+                <strong v-html="message.title"></strong>
+              </v-col>
+
+              <v-col
+                v-if="message.excerpt"
+                class="grey--text text-truncate hidden-sm-and-down"
+              >
+                &mdash;
+                {{ message.excerpt }}
+              </v-col>
+            </v-row>
+          </v-expansion-panel-header>
+
+          <v-expansion-panel-content>
+            <v-divider></v-divider>
+            <v-card-text v-text="lorem"></v-card-text>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      messages: [
+        {
+          avatar: 'https://picsum.photos/1920/1080?random=1',
+          name: 'suzucan2020',
+          title: 'Our Skill',
+          excerpt: 'Thank you for joining our community...',
+        },
+        {
+          color: 'red',
+          icon: 'mdi-account-multiple',
+          name: 'Social',
+          new: 1,
+          total: 3,
+          title: 'Twitter',
+        },
+        {
+          color: 'teal',
+          icon: 'mdi-tag',
+          name: 'Promos',
+          new: 2,
+          total: 4,
+          title: 'Shop your way',
+          exceprt: 'New deals available, Join Today',
+        },
+      ],
+      lorem: 'AWS, Kubernetes, Docker, Terraform, Chalice, Python, Flask, Django, C, C++, Nuxt, Vuetify, Github Actions',
+      }
+    },
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -17,123 +17,38 @@
       </v-carousel>
     </v-layout>
 
-    <v-layout row wrap>
-
-  <v-container fluid>
-    <v-row justify="center">
-      <v-subheader>Our Team</v-subheader>
-
-      <v-expansion-panels popout>
-        <v-expansion-panel
-          v-for="(message, i) in messages"
-          :key="i"
-          hide-actions
-        >
-          <v-expansion-panel-header>
-            <v-row
-              align="center"
-              class="spacer"
-              no-gutters
-            >
-              <v-col
-                cols="4"
-                sm="2"
-                md="1"
-              >
-                <v-avatar
-                  size="36px"
-                >
-                  <img
-                    v-if="message.avatar"
-                    alt="Avatar"
-                    src="https://picsum.photos/1920/1080?random=6"
-                  >
-                  <v-icon
-                    v-else
-                    :color="message.color"
-                    v-text="message.icon"
-                  ></v-icon>
-                </v-avatar>
-              </v-col>
-
-              <v-col
-                class="hidden-xs-only"
-                sm="5"
-                md="3"
-              >
-                <strong v-html="message.name"></strong>
-                <span
-                  v-if="message.total"
-                  class="grey--text"
-                >
-                  &nbsp;({{ message.total }})
-                </span>
-              </v-col>
-
-              <v-col
-                class="text-no-wrap"
-                cols="5"
-                sm="3"
-              >
-                <v-chip
-                  v-if="message.new"
-                  :color="`${message.color} lighten-4`"
-                  class="ml-0 mr-2 black--text"
-                  label
-                  small
-                >
-                  {{ message.new }} new
-                </v-chip>
-                <strong v-html="message.title"></strong>
-              </v-col>
-
-              <v-col
-                v-if="message.excerpt"
-                class="grey--text text-truncate hidden-sm-and-down"
-              >
-                &mdash;
-                {{ message.excerpt }}
-              </v-col>
-            </v-row>
-          </v-expansion-panel-header>
-
-          <v-expansion-panel-content>
-            <v-divider></v-divider>
-            <v-card-text v-text="lorem"></v-card-text>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+    <v-row>
+      <OurTeam />
     </v-row>
-  </v-container>
- 
-    </v-layout>
 
     <v-row justify="center">
       <v-subheader>Demonstration !</v-subheader>
     </v-row>
 
-    <v-layout row wrap>
-      <v-flex>
+    <v-row>
+      <v-col>
         <SampleSiteList />
-      </v-flex>
-      <v-flex>
+      </v-col>
+      <v-col>
         <VuetifyUIComponentList />
-      </v-flex>
-      <v-flex>
+      </v-col>
+      <v-col>
         <NuxtPracticeList />
-      </v-flex>
-    </v-layout>
+      </v-col>
+    </v-row>
 
   </v-container>
 
 </template>
 
 <script>
+  import OurTeam from "@/components/OurTeam.vue";
   import SampleSiteList from "@/components/SampleSiteList.vue";
   import VuetifyUIComponentList from "@/components/VuetifyUIComponentList.vue";
   import NuxtPracticeList from "@/components/NuxtPracticeList.vue";
   export default {
     components: {
+      OurTeam,
       SampleSiteList,
       VuetifyUIComponentList,
       NuxtPracticeList
@@ -157,32 +72,6 @@
             src: 'https://picsum.photos/1920/1080?random=5',
           },
         ],
-      messages: [
-        {
-          avatar: 'https://picsum.photos/1920/1080?random=6',
-          name: 'suzucan2020',
-          title: 'Our Skill',
-          excerpt: 'Thank you for joining our community...',
-        },
-        {
-          color: 'red',
-          icon: 'mdi-account-multiple',
-          name: 'Social',
-          new: 1,
-          total: 3,
-          title: 'Twitter',
-        },
-        {
-          color: 'teal',
-          icon: 'mdi-tag',
-          name: 'Promos',
-          new: 2,
-          total: 4,
-          title: 'Shop your way',
-          exceprt: 'New deals available, Join Today',
-        },
-      ],
-      lorem: 'AWS, Kubernetes, Docker, Terraform, Chalice, Python, Flask, Django, C, C++, Nuxt, Vuetify, Github Actions',
       }
     },
   }
